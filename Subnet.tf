ac9b918ced8b2505f0f3fe7040a77674d5ed479a
kits26kat24 <k.tijani@hotmail.com>
2023-07-26 01:48:49 +0100

    Test4B Assigment

--- Subnet.tf
+++ Subnet.tf
@@ -1,7 +1,7 @@
 # Creating A Subnet 
 # Resource: aws_subnet
 # https://registry.terraform.io/providers/hashicorp/aws/3.73.0/docs/resources/subnet
-resource "aws_subnet" "Amazon_Linux_Main" {
+resource "aws_subnet" "Dev_Ec2_Main_One" {
   vpc_id                  = aws_vpc.test4bcase_main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
@@ -10,30 +10,30 @@ resource "aws_subnet" "Amazon_Linux_Main" {
 
   availability_zone = "eu-west-1a"
   tags = {
-    Name = "Amazon-Linux"
+    Name = "Dev_1"
   }
 }
-# resource "aws_subnet" "Redhat_Main" {
-#   vpc_id                  = aws_vpc.test4bcase_main.id
-#   cidr_block              = "10.0.2.0/24"
-#   map_public_ip_on_launch = "true"
+resource "aws_subnet" "Dev_Ec2_Main_Two" {
+  vpc_id                  = aws_vpc.test4bcase_main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = "true"
 
-#   # AZ for the subnet.
+  # AZ for the subnet.
 
-#   availability_zone = "eu-west-1b"
-#   tags = {
-#     Name = "Redhat"
-#   }
-# }
-# resource "aws_subnet" "Ubuntu_Main" {
-#   vpc_id                  = aws_vpc.test4bcase_main.id
-#   cidr_block              = "10.0.3.0/24"
-#   map_public_ip_on_launch = "true"
+  availability_zone = "eu-west-1b"
+  tags = {
+    Name = "Dev_2"
+  }
+}
+resource "aws_subnet" "Sit_Ec2_Main" {
+  vpc_id                  = aws_vpc.test4bcase_main.id
+  cidr_block              = "10.0.3.0/24"
+  map_public_ip_on_launch = "true"
 
-#   # AZ for the subnet.
+  # AZ for the subnet.
 
-#   availability_zone = "eu-west-1c"
-#   tags = {
-#     Name = "Ubuntu-Main"
-#   }
-# }
+  availability_zone = "eu-west-1b"
+  tags = {
+    Name = "Sit_1"
+  }
+}
